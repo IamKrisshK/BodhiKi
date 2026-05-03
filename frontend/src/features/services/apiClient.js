24280b2d60c73769fbe341c6c6ba1b651ca96ed9
Krissh K. <krissh.k2005@gmail.com>
2026-05-03 22:17:24 +0530

    Update apiClient.js

--- frontend/src/features/services/apiClient.js
+++ frontend/src/features/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bodhi-ki-backend.vercel.app/api/",
+  baseURL: "https://bodhi-ki-backend.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
